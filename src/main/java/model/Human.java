ecc5b86dd9ff747d76c47b0467a05fb7ee927608
Nahid Asadioun <nahidasadioun@gmail.com>
2019-05-20 00:06:13 +0430

    src/: just create class Human,Student,Teacher,programmer in package model

---
+++ src/main/java/model/Human.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Human {
+   private int id ;
+   private int age;
+   private String sex;
+   private double salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
