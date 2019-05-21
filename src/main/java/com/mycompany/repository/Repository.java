025dfd58d13a8b7ec3110c5df63c6344319ee520
Nahid Asadioun <nahidasadioun@gmail.com>
2019-05-21 19:40:00 +0430

    repository:impelements Repository

--- src/main/java/com/mycompany/repository/Repository.java
+++ src/main/java/com/mycompany/repository/Repository.java
@@ -1,4 +1,10 @@
 package com.mycompany.repository;
 
+import com.mycompany.model.Human;
+
 public interface Repository {
+     Human read(Human human);
+     void write(Human human);
+     void printall(Human human);
+
 }
