3353e7b7b5dea3484261d5f0782e94af65003e4c
Nahid Asadioun <nahidasadioun@gmail.com>
2019-05-21 19:41:20 +0430

    student repository: impelements Repository

--- src/main/java/com/mycompany/repository/StudentRepository.java
+++ src/main/java/com/mycompany/repository/StudentRepository.java
@@ -1,4 +1,21 @@
 package com.mycompany.repository;
 
-public class StudentRepository {
+import com.mycompany.model.Human;
+
+public class StudentRepository implements Repository {
+
+    @Override
+    public Human read(Human human) {
+        return null;
+    }
+
+    @Override
+    public void write(Human human) {
+
+    }
+
+    @Override
+    public void printall(Human human) {
+
+    }
 }
