332d477ef52e3430b83c293f1437de6a71e0b716
Nahid Asadioun <nahidasadioun@gmail.com>
2019-05-22 12:02:03 +0430

    mainproject:no change

--- src/main/java/com/mycompany/mainproject/MainProject.java
+++ src/main/java/com/mycompany/mainproject/MainProject.java
@@ -1,62 +1,48 @@
 package com.mycompany.mainproject;
 
-import com.mycompany.model.Human;
-import com.mycompany.model.Programer;
 import com.mycompany.model.Student;
-import com.mycompany.model.Teacher;
+import com.mycompany.repository.Repository;
+import com.mycompany.repository.StudentRepository;
+import com.mycompany.repository.TeacherRepository;
 import utilities.CSV;
 
-
-import java.util.LinkedList;
 import java.util.List;
 
 public class MainProject {
     public static void main(String[] args) {
 
-        List<Human> humans = new LinkedList<Human>();
-
         String fileName = "/home/amir/javarepository/src/main/resources/users.csv";
+
+
+        Repository studentRepository = new StudentRepository();
+
+
+        Repository teacherRepository = new TeacherRepository();
+
 
 
         //read a CSV file then create new  on that data
 
         List<String[]> newHolder = CSV.read(fileName);
         for (String[] holder : newHolder) {
-            System.out.println(holder[0]);
-            System.out.println(holder[1]);
-            System.out.println(holder[2]);
-            System.out.println(holder[3]);
-            System.out.println(holder[4]);
 
-            String Id = holder[0];
+            int id = Integer.valueOf(holder[0]);
             String type = holder[1];
-            String age = holder[2];
+            int age = Integer.valueOf(holder[2]);
             String sex = holder[3];
             double salary = Double.parseDouble(holder[4]);
-            System.out.println(Id + " " + type + " " + age + " "+ sex + " "+ salary);
+            System.out.println(id + " " + type + " " + age + " "+ sex + " "+ salary);
 
             if ("S".equals(type)){
                 System.out.println("Open Student");
-                humans.add(new Student(Id,age,sex,salary));
-            }
-            else if (type.equals("T")) {
-                System.out.println("open Teacher");
-                humans.add(new Teacher(Id,age,sex,salary));
-            }
-             else if (type.equals("P")){
-                System.out.println("open Programmer");
-                humans.add(new Programer(Id,age,sex,salary));
+                studentRepository.write(new Student(id,age,sex,salary));
+            } //TODO:impliment else if
 
-            }else {
-                System.out.println(" ERROR reading type");
-            }
         }
-
-        for (Human human : humans){
-            System.out.println("\n *********************");
-            human.showInfo();
-        }
-
+        studentRepository.printall();
+        System.out.println(studentRepository.read(7).toString());
 
     }
+
+
 }
