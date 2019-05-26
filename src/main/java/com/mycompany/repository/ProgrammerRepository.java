d59331eab4f16a5195edf5ce39f7d85146749bd4
Nahid Asadioun <nahidasadioun@gmail.com>
2019-05-27 00:29:12 +0430

    ProgrammerRepository: add method read , write , print all with hash map and iterator

--- src/main/java/com/mycompany/repository/ProgrammerRepository.java
+++ src/main/java/com/mycompany/repository/ProgrammerRepository.java
@@ -2,23 +2,34 @@ package com.mycompany.repository;
 
 import com.mycompany.model.Human;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class ProgrammerRepository implements Repository {
 
+   private HashMap<Integer,Human > programmersMap = new HashMap<Integer, Human>();
 
 
     @Override
     public Human read(int id) {
-        return null;
+       return programmersMap.get(id);
+
     }
 
     @Override
     public void write(Human human) {
-
+    programmersMap.put(human.getId(),human);
 
     }
 
     @Override
     public void printall() {
-
+        System.out.println(programmersMap.size());
+        Iterator iterator = programmersMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            System.out.println( pair.getValue().toString());
+        }
     }
 }
